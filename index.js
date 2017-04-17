a3d2103d2dbf87fcb7e514454e463a7bf8b94ccb
kazikai <kazikai@github.com>
2017-04-17 22:57:24 +0900

    month bug fix

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ var timestamp = function(dateObject, pattern){
       hour = (dateObject.getHours() < 10 ? '0' : '') + dateObject.getHours(),
       min = (dateObject.getMinutes() < 10 ? '0' : '') + dateObject.getMinutes(),
       sec = (dateObject.getSeconds() < 10 ? '0' : '') + dateObject.getSeconds(),
-      month = (+dateObject.getMonth() + 1 < 10 ? '0' : '') + dateObject.getMonth() + 1,
-      day = (dateObject.getDate() < 10 ? '0' : '') + dateObject.getDate();
+      month = dateObject.getMonth() < 9 ? '0' + (dateObject.getMonth() + 1)  : dateObject.getMonth() + 1,
+      day = dateObject.getDate() < 10 ? ('0' + dateObject.getDate()) : dateObject.getDate(); 
+ 
   var result = year + month + day + '_' + hour + min + sec;
   if (pattern === 'yyyy-mm-dd') {
     result = year + '-' + month + '-' + day;
